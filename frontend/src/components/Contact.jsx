9071696cd235e41f4f94d06008ba370b8d1a91cd
Diwakar <you@example.com>
2025-01-12 23:16:51 +0530

    iihkh

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
   let[contact,setContact]=useState();
   let contact_names=['Github','Linked In','Email','Call'];
   let bg=['bg-social'];
-  let icons=['github','linkedin','gmail','whatsapp'];
+  let icons=['fa-brands fa-github','fa-brands fa-linkedin','fa fa-envelope','fa-brands fa-whatsapp'];
   let usernames=['diwadv08','dv813','diwadv813','9489460029'];
   let contact_links=['https://github.com/diwadv08','https://www.linkedin.com/in/dv813','mailto:'+usernames[2]+'@gmail.com','tel:'+usernames[3]];
 
@@ -40,7 +40,7 @@ function Contact() {
                           <div className={"card "+bg[0]+" py-md-3 py-0"}>
                             <a href={contact_links[index]} target='_blank'>
                               <div className="d-flex justify-content-center align-items-center">
-                                <h1 style={{fontSize:'37px'}}><i className={'fa-brands fa-'+icons[index]+' me-2'}></i></h1>
+                                <h1 style={{fontSize:'37px'}}><i className={icons[index]+' me-2'}></i></h1>
                                 <h6 style={{textTransform:'lowercase'}} className='mt-2'>{usernames[index]}</h6>
                               </div>
                             </a>
